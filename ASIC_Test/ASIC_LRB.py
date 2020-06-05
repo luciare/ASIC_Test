c294495870ed72187ac22b3fc3f797893410a1e0
luciare <lucia.re@imb-cnm.csic.es>
2020-06-05 13:35:08 +0200

    Avanze Tree_LRB y signle acquisition
    sigue dandome error el import _ok que se realiza en el fichero ok.py

---
+++ ASIC_Test/ASIC_LRB.py
@@ -0,0 +1,167 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jun  5 10:55:11 2020
+
+@author: lucia
+"""
+
+from __future__ import print_function
+import os
+
+import numpy as np
+import matplotlib.pyplot as plt
+import time
+
+from PyQt5 import Qt
+
+from pyqtgraph.parametertree import Parameter, ParameterTree
+
+import Trees.ASICTree_LRB as ASICConfig
+
+import Functions.FileModule as FileMod
+
+
+##ASIC
+# import JoseCodes.BC_COM_AS2_V3 as AS
+
+
+class MainWindow(Qt.QWidget):
+    ''' Main Window '''
+    def __init__(self):
+        # 'Super' is used to initialize the class from which this class
+        # depends, in this case MainWindow depends on Qt.Widget class
+        super(MainWindow, self).__init__()
+    # buscar esto que son cosas de la ventana de la gui
+        self.setFocusPolicy(Qt.Qt.WheelFocus)
+        layout = Qt.QVBoxLayout(self)
+        # Qt.QPushButton is used to generate a button in the GUI
+        self.btnStart = Qt.QPushButton("Start Gen and Adq!")
+        layout.addWidget(self.btnStart)
+        
+        self.btnStartSingleAdq = Qt.QPushButton("Single Adq!")
+        layout.addWidget(self.btnStartSingleAdq)
+
+# #############################Save##############################
+        self.SaveStateParams = FileMod.SaveTreeSateParameters(QTparent=self,
+                                                              name='State')
+        # With this line, it is initize the group of parameters that are
+        # going to be part of the full GUI
+        self.Parameters = Parameter.create(name='params',
+                                           type='group',
+                                           children=(self.SaveStateParams,))
+# #############################File##############################
+        self.FileParams = FileMod.SaveFileParameters(QTparent=self,
+                                                     name='Record File')
+        self.Parameters.addChild(self.FileParams)
+
+# #############################ASIC##############################
+        self.ASICParams = ASICConfig.ASICParameters(QTparent=self,
+                                                    name='ASIC Configuration')
+        self.Parameters.addChild(self.ASICParams)
+
+        # self.ASIC_C = AS.ASIC(DEBUG =1)
+                
+# ############################GuiConfiguration##############################
+        # Is the same as before functions but for 'Parameters' variable,
+        # which conatins all the trees of all the Gui, so on_Params_changed
+        # will be execute for any change in the Gui
+        self.Parameters.sigTreeStateChanged.connect(self.on_Params_changed)
+    # EXPLICAR ESTO TAMBIEN QUE TIENE QUE VER CON LA GUI
+        self.treepar = ParameterTree()
+        self.treepar.setParameters(self.Parameters, showTop=False)
+        self.treepar.setWindowTitle('pyqtgraph example: Parameter Tree')
+
+        layout.addWidget(self.treepar)
+
+        self.setGeometry(550, 10, 300, 700)
+        self.setWindowTitle('MainWindow')
+        # It is connected the action of click a button with a function
+        self.btnStart.clicked.connect(self.on_btnStart)
+        
+        #Boton adicional
+        self.btnStartSingleAdq.clicked.connect(self.on_btnStartSingleAdq)
+
+# ############################Changes Control##############################
+    def on_Params_changed(self, param, changes):
+        '''
+        This function is used to print in the consol the changes that have
+        been done.
+
+        '''
+        print("tree changes:")
+        for param, change, data in changes:
+            path = self.Parameters.childPath(param)
+            if path is not None:
+                childName = '.'.join(path)
+            else:
+                childName = param.name()
+        print('  parameter: %s' % childName)
+        print('  change:    %s' % change)
+        print('  data:      %s' % str(data))
+        print('  ----------')
+        
+# ############################START##############################
+    def on_btnStart(self):
+        '''
+        This function is executed when the 'start' button is pressed. It is
+        used to initialize the threads, emit signals and data that are
+        necessary durint the execution of the program. Also in this function
+        the different threads starts.
+
+        '''
+        
+        print('started')
+        
+# ############################Single Adquisition Long##############################
+    def on_btnStartSingleAdq(self):
+        '''
+        This function is executed when the 'start' button is pressed. It is
+        used to initialize the threads, emit signals and data that are
+        necessary durint the execution of the program. Also in this function
+        the different threads starts.
+
+        '''
+        print('########## Starting single Adq ##########')   
+        self.GenKwargs = self.ASICParams.GetGenParams()
+        print(self.GenKwargs)
+        #Actualizacion de los parametros de configuracion
+        # DIC_C = self.ASICParams.GetGenParams()
+        # DIC_R = self.ASICParams.GetRowsParams()
+    
+        # self.ASIC_C.Dict_To_InstructionSimple(DIC_C)
+        # self.ASIC_C.Dict_To_InstructionSimple(DIC_R)
+        
+        # #Short Run Adquisicion
+        # self.Error,self.Col,self.Sar_0,self.Sar_1,self.Sar_2,self.Sar_3 = self.ASIC_C.ReadAcqS()
+        
+    
+        # sense FPGA carregar dades:
+        
+        # Sars = np.array(self.Sar_0, self.Sar_1, self.Sar_2, self.Sar_3)#no se si esta bien configurado el array
+        # for ind, Sar in enumerate(Sars):
+        # # convertir sars
+        #     Sars_Data, Y_Data = AS.Conversion(Data=Sar,
+        #                                       FS = self.GenKwargs["FS"])
+        # # cridat plots ams row data (probar amb plt.plot())
+        #     plt.figure('Sar'+str(ind))
+        #     plt.plot(Sars_Data, Y_Data)
+        
+        # desentrallar amb la funcio: 
+        
+        # cridar plots amd data desentrallada (probar amb plt.plot())
+        
+        # Save amb FileModule
+        
+        print('########## Starting single Adq ##########')      
+
+        
+        
+        
+        
+# ############################MAIN##############################
+
+if __name__ == '__main__':
+    app = Qt.QApplication([])
+    mw = MainWindow()
+    mw.show()
+    app.exec_()
--- ASIC_Test/Trees/ASICTree_LRB.py
+++ ASIC_Test/Trees/ASICTree_LRB.py
@@ -10,6 +10,33 @@ import pyqtgraph.parametertree.Parameter as pParams
 
 import numpy as np
 
+Clk_Freq_D_Lw ={#Freq: M, D
+                "54MHz": (27    ,   25), 
+                "28MHz": (14    ,   25), 
+                "27MHz": (27    ,   50),   
+                "26MHz": (13    ,   25),   
+                "25MHz": (26    ,   50),  
+                "13MHz": (13    ,   50), 
+                "6.5MHz": (13    ,   25), 
+                "1MHz": (6    ,   50), 
+                }
+    
+Gain_AFE_Lw = { 2 : 0,
+                4 : 1,
+                8 : 2,
+                16 :3   
+             }
+
+Dic_PB_Lw ={4: (0,0),   
+            8: (0,1),   
+            16: (1,0),
+            32:(1,1)
+            }
+
+Clk_Freq_D_L = list(Clk_Freq_D_Lw.keys())
+Gain_AFE_L = list(Gain_AFE_Lw.keys())
+Dic_PB_L = list(Dic_PB_Lw.keys())
+    
 GeneratorConfig =  {'name': 'GeneratorConfig',
                     'type': 'group',
                     'children':({'name': 'FsClock',
@@ -64,10 +91,10 @@ RowConf = {'name': 'RowConfig',
                                        'values':Gain_AFE_L,
                                        'value': 2,},
                                      {'name':'Offset Vector',
-                                      'type': 'list', 
-                                      'readonly': True,
-                                      'values': [0,0,0,0,1],##La lista deberia de ser hasta 32 posiciones
-                                      'value': [0,0,0]})}, #aqui va el valor predeterminado de la lista
+                                      'type': 'str', 
+                                      'readonly': False,
+                                      'value': "0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0"}
+                                     )}, 
                         # {'name':'Row 1',
                         #  'type': 'group',
                         #  'children':({'name': 'Enable',
@@ -121,8 +148,29 @@ class ASICParameters(pTypes.GroupParameter):
         self.FsClock = self.GenConfig.param('FsClock')
         self.addChild(RowConf)
         self.RowsConfig = self.param('RowConfig')
-        self.GenConfig.sigTreeStateChanged.connect()
+        self.RowsConfig.sigTreeStateChanged.connect(self.on_RowsConfig_changed)
         self.DACEL.sigValueChanged.connect(self.on_DACEL_changed)
+        
+    def on_RowsConfig_changed(self):
+        for pars in self.RowsConfig.children():
+            if pars.param('Enable').value():
+                self.OffVectStr = ""
+                self.OffVect = pars.param('Offset Vector').value().split(",")
+                for ind in self.OffVect:
+                    if int(ind) < 0:
+                        self.OffVect[self.OffVect.index(ind)] = '0'
+                    elif int(ind) > 6:
+                        self.OffVect[self.OffVect.index(ind)] = '6'
+
+                while len(self.OffVect) < 32:
+                    print("Less than 32 values, adding 0s")
+                    self.OffVect.append('0')
+                while len(self.OffVect) > 32:
+                    print("More than 32 values, removing last values")
+                    self.OffVect.remove(self.OffVect.index(len(self.OffVect)))
+                
+                self.OffVectStr = ','.join(self.OffVect)
+                pars.param('Offset Vector').setValue(self.OffVectStr)
 
     def on_DACEL_changed(self):
         if self.DACEL.value() > 1.8:
@@ -140,31 +188,36 @@ class ASICParameters(pTypes.GroupParameter):
                     "6.5MHz": (13    ,   25),
                     "1MHz": (6    ,   50),
                     }
-        FsTuple = Clk_Freq_D[self.FsClock]
-        return FsTuple
-    
+        
+        FsTuple = Clk_Freq_D[self.FsClock.value()]
+        FS = int(self.FsClock.value().replace('MHz',''))*10**6
+        return FS, FsTuple
+
     def GetGenParams(self):
         self.Generator = {}
         for Config in self.GenConfig.children():
-           self.Generator[Config.name()] = Config.value()
-           
+            #se puede implementar este if else para guardar directamente el tuple
+            if Config.name() == 'FsClock':
+                self.Generator["FS"], self.Generator[Config.name()] = self.GetFsClock()
+            else:
+                self.Generator[Config.name()] = Config.value()
+
         return self.Generator
-    
+
     def GetRowsParams(self):
-        # self.Rows = {}
-        # for Config in self.RowsConfig.children():
-        #     for Values in Config.children():
-        #         self.Rows[Config.name()][Values.name()] = Values.value()
-        
         self.Rows = {}
         for Config in self.RowsConfig.children():
             Rows_S ={}
             for Values in Config.children():
-                Rows_S[Values.name()] = Values.value()
+                if Values.name() == 'Offset Vector':
+                    Rows_S[Values.name()] = Values.value().split(",")
+
+                else:
+                    Rows_S[Values.name()] = Values.value()
             self.Rows[Config.name()] = Rows_S
-        
+
         return self.Rows
-    
+
     def GetRowsParamsDIC(self):
         
         return self.GenConfig
